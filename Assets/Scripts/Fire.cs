5d5a1b9a814b9fdff8cbfde819a8d2b6fc0eab23
omarcosvbn <omarcosvbn@gmail.com>
2024-04-11 12:49:43 +0100

    novos sistemas

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -1,13 +1,45 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class Fire : MonoBehaviour{
 
-    public bool onFire = true;
+    public Slider temperature;
+    public bool onFire;
+    public float chance;
+
+    void Start(){
+            onFire = false;
+            GetComponent<Renderer>().material.color = new Color(0, 255, 0);
+            temperature.value = 0.15f;
+    }
+
+    void Update(){
+        //velocidade em que os incendios sobem a barra de temperatura
+        if(onFire == true){
+            temperature.value += 0.0005f * Time.deltaTime;
+        }
+
+        //apagar os incendios para testar
+        if (Mouse.current.leftButton.wasPressedThisFrame){
+            onFire = false;
+            GetComponent<Renderer>().material.color = new Color(0, 255, 0);
+        }
+
+        //criar incendios com base no valor do slider
+        chance = temperature.value * 100;
+        if(Random.Range(1, 100) < chance){
+            onFire = true;
+            GetComponent<Renderer>().material.color = new Color(255, 0, 0);
+        }
+    }
 
     public void SetOnFire(bool value){
         onFire = value;
+    }
+
+    public void OnTriggerEnter(){
+        onFire = false;
+        GetComponent<Renderer>().material.color = new Color(0, 255, 0);
     }
 }
