6a7d0f32f3eb3e845086c3f91d82518357a46f20
omarcosvbn <omarcosvbn@gmail.com>
2024-04-29 13:56:13 +0100

    new map system

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -2,18 +2,18 @@ using UnityEngine;
 
 public class Button : MonoBehaviour {
     public bool isPressed;
+    private bool alreadyPressed;
 
     private void OnTriggerEnter(Collider other){
+        if (!alreadyPressed){
             isPressed = true;
+            alreadyPressed = true;
+        }
+        isPressed = false;
     }
 
     private void OnTriggerExit(Collider other){
             isPressed = false;
+            alreadyPressed = false;
     }
-
-
-
-   void Update(){
-    //Debug.Log(isPressed);
-   }
 }
--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -7,15 +7,26 @@ public class Fire : MonoBehaviour{
 
     public Slider temperature;
     [SerializeField] GameObject button;
+    [SerializeField] GameObject buttonUp;
+    [SerializeField] GameObject buttonDown;
+    
     private Button buttonScript;
+    private Button buttonUpScript;
+    private Button buttonDownScript;
+
     public bool onFire;
     public float chance;
+
+    [SerializeField] GameObject bigCollider;
+    [SerializeField] GameObject smallCollider;
 
     void Start(){
         onFire = false;
         StartCoroutine(IncrementTemperature());
         StartCoroutine(RandomFireGeneration());
         buttonScript = button.GetComponent<Button>();
+        buttonUpScript = buttonUp.GetComponent<Button>();
+        buttonDownScript = buttonDown.GetComponent<Button>();
     }
 
     IEnumerator IncrementTemperature(){
@@ -44,6 +55,16 @@ public class Fire : MonoBehaviour{
         //mudar cor para teste
         if(onFire == true) GetComponent<Renderer>().material.color = new Color(255, 0, 0);
         else GetComponent<Renderer>().material.color = new Color(0, 255, 0);
+
+        if(buttonUpScript.isPressed == true){
+            if(smallCollider.transform.position.x < 0.08f) smallCollider.transform.position = new Vector3 (smallCollider.transform.position.x + 0.04f, 0, 0);
+            if(bigCollider.transform.position.x < 40f) bigCollider.transform.position = new Vector3 (bigCollider.transform.position.x + 20f, 20, 0);
+        }
+
+        if(buttonDownScript.isPressed == true){
+            if(smallCollider.transform.position.x > 0f) smallCollider.transform.position = new Vector3 (smallCollider.transform.position.x - 0.04f, 0, 0);
+            if(bigCollider.transform.position.x > 0f) bigCollider.transform.position = new Vector3 (bigCollider.transform.position.x - 20f, 20, 0);
+        }
     }
 
     private void OnTriggerStay(Collider other){
