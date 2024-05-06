72f334ddde652a935000a910ff9f88167c5346ca
omarcosvbn <omarcosvbn@gmail.com>
2024-05-06 15:52:19 +0100

    novos botoes

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -2,17 +2,32 @@ using UnityEngine;
 
 public class Button : MonoBehaviour {
     public bool isPressed;
+    private bool canPress = true; // Add a flag to indicate if the button can be pressed again
+    [SerializeField] GameObject botao;
+    private Vector3 originalPosition;
+    private float pressedOffset = -0.009f; // Offset to move the button down when pressed
+
+    private void Start() {
+        originalPosition = botao.transform.position;
+    }
 
     private void OnTriggerEnter(Collider other){
-        if (!other.CompareTag("Base")) {
+        if (!other.CompareTag("Base") && canPress) { // Check if the button can be pressed
             isPressed = true;
+            canPress = false; // Set canPress to false to prevent repeated pressing
+            Debug.Log("Button pressed");
+            // Move down when pressed
+            botao.transform.position += new Vector3(0, pressedOffset, 0);
         }
-        transform.position = new Vector3(0, 0, 0);
     }
 
     private void OnTriggerExit(Collider other){
         if (!other.CompareTag("Base")) {
             isPressed = false;
+            canPress = true; // Reset the canPress flag when the button is released
+            Debug.Log("Button released");
+            // Return to the original position
+            botao.transform.position = originalPosition;
         }
     }
 }
